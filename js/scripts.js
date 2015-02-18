0ac791409296d8d4ba7f758ae56bdb6b175252c1
Benjamin Herson <benjaminherson@gmail.com>
2015-02-17 17:00:43 -0800

    add styling makeover

--- js/scripts.js
+++ js/scripts.js
@@ -14,17 +14,28 @@ $(document).ready(function () {
       weight: weight1,
       distance: distance1,
       cost: function() {
-        var area = (this.length * this.width * this.height);
-        if (area <= 200){
-          return 7.50
+        var volume = (this.length * this.width * this.height);
+        var vcost;
+        var wcost;
+        var dcost = this.distance * .01;
+        if (volume <= 200){
+          vcost = 7.50;
         } else {
-          return (area * .0375)
+          vcost = (volume * .0375)
         }
-
+        if (this.weight > 70) {
+          wcost = this.weight * 2;
+        } else {
+          wcost = this.weight / 2;
+        }
+        if (this.distance > 4000) {
+          dcost = dcost + 50;
+        }
+        return (vcost + dcost + wcost).toFixed(2);
       }
     };
 
-
+    $("#priceDiv").show();
     $("#priceSpan").text(parcel.cost());
 
     event.preventDefault();
